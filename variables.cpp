1b7681927ec59b39d5f0a044ebdfc1868689141c
akumaru <akumaru.senju@gmail.com>
2022-11-29 20:26:56 +0500

    done: variables tutorial

---
+++ variables.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main(){
+
+	// integer data type
+
+	int year = 2022;
+	int age = 16;
+	int days = 7.5;
+
+	// double data type
+
+	double price = 9.99;
+	double gpa = 2.5;
+	double temperature = 25.1;
+
+	// char data type
+
+	char grade = 'A';
+	char initial = 'C';
+
+	// boolean data type
+
+	bool student = true;
+	bool power = false;
+	bool for_sale = true;
+
+	//string data types
+
+	std::string name = "akumaru";
+	std::string day = "Tuesday";
+	std::string food = "Сырок";
+
+	std::cout << "Мне нравится " << food << std::endl;
+	std::cout << "Мне " << age << std::endl;
+
+	return 0;
+}
