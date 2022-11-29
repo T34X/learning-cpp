6a48267f258b756240fd6598ef9f25f58e669ac6
akumaru <akumaru.senju@gmail.com>
2022-11-29 20:31:17 +0500

    done: constants tutorial

---
+++ constants.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+	const double PI = 3.14159;
+	PI = 3.1416; // throws an error
+	const int LIGHT_SPEED = 29898989;
+	const int WIDTH = 1600;
+	const int HEIGHt = 900;
+
+	std::cout << circumference << " cm" << std::endl;
+
+	return 0;
+}
