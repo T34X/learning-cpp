785b0c2b26cb8ae4af45da37c7f5f06630f46d1a
akumaru <akumaru.senju@gmail.com>
2022-12-04 16:33:27 +0500

    done: booleans tutorial

---
+++ booleans.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	bool isCPPGreat = true;
+	bool isPythonSheet = false;
+
+	std::cout << isCPPGreat << "\n";
+	std::cout << isPythonSheet << "\n";
+
+	return 0;
+}
